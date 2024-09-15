11dc89582008ac07623cf4d235c4e64a79603e51
Peter Gallwas <peter@husky.nz>
2024-09-15 20:46:42 +1200

    updarte studff

--- src/lib/rurl.ts
+++ src/lib/rurl.ts
@@ -5,12 +5,14 @@ export type PredefinedUrls = {
 export const predefinedUrls: PredefinedUrls = {
     logo: 'https://serv.husky.nz/logo/default.png',
     github: 'https://github.com/huskynz/',
-    panel: 'https://panel.hnz.li',
+    panel: 'https://panel.dignation.nz',
     twitch: 'https://www.twitch.tv/huskynzofficial',
     youtube: 'https://www.youtube.com/@huskynz',
     ghm: 'https://github.com/huskynz/10',
     m: 'https://www.husky.nz',
     blog: 'https://www.husky.nz/blog',
     sh: 'https://panel.serverhorse.com/',
-    w7su: 'https://download.microsoft.com/download/C/8/7/C87AE67E-A228-48FB-8F02-B2A9A1238099/Windows6.1-KB3033929-x64.msu'
+    w7su: 'https://download.microsoft.com/download/C/8/7/C87AE67E-A228-48FB-8F02-B2A9A1238099/Windows6.1-KB3033929-x64.msu',
+    gurl: 'https://github.com/huskynz/url-shortner/'
+    sp: 'https://open.spotify.com/'
 };
