18f15049ca01b2fb332b7c42f065e01016ff13c0
Peter Gallwas <peter@husky.nz>
2024-09-24 10:41:44 +1200

    Add inital url page

--- src/lib/rurl.ts
+++ src/lib/rurl.ts
@@ -13,6 +13,7 @@ export const predefinedUrls: PredefinedUrls = {
     blog: 'https://www.husky.nz/blog',
     sh: 'https://panel.serverhorse.com/',
     w7su: 'https://download.microsoft.com/download/C/8/7/C87AE67E-A228-48FB-8F02-B2A9A1238099/Windows6.1-KB3033929-x64.msu',
-    gurl: 'https://github.com/huskynz/url-shortner/'
-    sp: 'https://open.spotify.com/'
+    gurl: 'https://github.com/huskynz/url-shortner/',
+    sp: 'https://open.spotify.com/',
+    urls: 'https://hnz.li/urls'
 };
