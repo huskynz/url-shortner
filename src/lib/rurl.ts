0a6d355d709af48509be1b3d324145d1db4761bd
Peter Gallwas <peter@husky.nz>
2024-09-25 13:22:14 +1200

    Change over panel and w7su to be a depraced link by useing the new page on www.husky.nz

--- src/lib/rurl.ts
+++ src/lib/rurl.ts
@@ -5,14 +5,14 @@ export type PredefinedUrls = {
 export const predefinedUrls: PredefinedUrls = {
     logo: 'https://serv.husky.nz/logo/default.png',
     github: 'https://github.com/huskynz/',
-    panel: 'https://panel.dignation.nz',
+    panel: 'https://www.husky.nz/invaildlink?name=/panel',
     twitch: 'https://www.twitch.tv/huskynzofficial',
     youtube: 'https://www.youtube.com/@huskynz',
     ghm: 'https://github.com/huskynz/10',
     m: 'https://www.husky.nz',
     blog: 'https://www.husky.nz/blog',
     sh: 'https://panel.serverhorse.com/',
-    w7su: 'https://download.microsoft.com/download/C/8/7/C87AE67E-A228-48FB-8F02-B2A9A1238099/Windows6.1-KB3033929-x64.msu',
+    w7su: 'http://www.husky.nz/invaildlink?name=/w7su',
     gurl: 'https://github.com/huskynz/url-shortner/',
     sp: 'https://open.spotify.com/',
     urls: 'https://hnz.li/urls'
