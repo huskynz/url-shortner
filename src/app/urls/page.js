a64180a9de878aecf863aa541e387116771a8eb5
Peter Gallwas <peter@husky.nz>
2025-02-05 11:52:55 +1300

    Innit v3 by removeing v2 code

--- src/app/urls/page.js
+++
@@ -1,8 +0,0 @@
-import UrlPageClient from './UrlPageClient';
-import { fetchDisplayUrls, logUserData } from '../utils';
-
-export default async function UrlsPage() {
-  const urls = await fetchDisplayUrls();
-  await logUserData("urls");
-  return <UrlPageClient initialUrls={urls} />;
-}
