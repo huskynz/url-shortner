af7c3820f090ab3a99dd357bebda07f59eb35721
Peter Gallwas <peter@husky.nz>
2025-06-15 23:11:23 +1200

    Delete supabase/migrations directory

--- supabase/migrations/20240320000001_add_password_to_urls.sql
+++
@@ -1,6 +0,0 @@
--- Add a password column to the urls table
-ALTER TABLE urls
-ADD COLUMN password TEXT;
-
--- If you need to encrypt existing passwords or set default values, do it here.
--- For new entries, the password column will be null by default if not provided. 
